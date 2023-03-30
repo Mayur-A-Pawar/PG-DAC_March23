00bd4d9c819c62216f0bdede534a36cc1d1c51c9
Mayur-A-Pawar <128139657+Mayur-A-Pawar@users.noreply.github.com>
2023-03-30 15:15:37 +0530

    Delete Assignment No 1 directory

--- Assignment No 1/solution/Q32/Q32.java
+++
@@ -1,9 +0,0 @@
-//32.Write a program to accept and print full name as an argument from command line.
-class Q32
-{
-    public static void main(String args[])
-    {
-        String s1=args[0];
-        System.out.println("Kasay BHAWA "+args[0]+" "+args[1]+" "+args[2]);
-    }
-}
